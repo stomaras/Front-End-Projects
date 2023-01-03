23e220c59d0990f066bfeba87a103eb8470a07ef
stomaras <sptoma724@gmail.com>
2023-01-03 15:46:02 +0200

    Direct feedback React To lOST Focus

--- Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/components/SimpleInput.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/components/SimpleInput.tsx
@@ -16,7 +16,14 @@ export const SimpleInput = () => {
         setEnteredName(event.target.value)
     }
 
-    
+    const nameInputBlurHandler = (event: React.FocusEvent<HTMLElement>) => {
+        setEnteredNameTouched(true);
+        if(enteredName.trim() == '') {
+            setEnteredNameIsValid(false);
+            return;
+        }
+
+    }
 
     const formSubmissionHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -38,7 +45,7 @@ export const SimpleInput = () => {
     <form onSubmit={formSubmissionHandler}>
         <div className={nameInputClasses}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+            <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
             {nameInputIsInvalid && <p>Name must not be empty.</p>}
         </div>
         <div className='form-actions'>

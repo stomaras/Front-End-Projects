8e027c68be427f07653dc06c584ab10f436d917c
stomaras <sptoma724@gmail.com>
2023-01-05 18:49:54 +0200

    Simple Input Section 16 Challenge email done and testing continued

--- Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/SimpleInput.spec.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/SimpleInput.spec.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import { render, RenderResult, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 let appWrapper: HTMLElement;
+let textBoxes: HTMLElement[];
+let textBoxName: HTMLElement;
+let textBoxEmail: HTMLElement;
+let submitButton: HTMLElement;
 
 beforeEach(() => {
     const wrapper = render(<App/>);
     appWrapper = wrapper.getByTestId('app-id');
-})
+    submitButton = screen.getByRole('button');
+    textBoxes = screen.getAllByRole('textbox');
+    textBoxName = textBoxes[0];
+    textBoxEmail = textBoxes[1];
+},5000)
 
 describe("Testing Infrastructure of Section 16", () => { 
     it('renders Simple Input Correctly', () => {
         expect(appWrapper).toBeInTheDocument();
-    });
+
+        const name = screen.getByText(/your name/i);
+        expect(name).toBeInTheDocument();
+
+        const email = screen.getByText(/your e-mail/i);
+        expect(email).toBeInTheDocument();
+
+        for (let index = 0; index < textBoxes.length; index++) {
+            const textBox = textBoxes[index];
+            expect(textBox).toBeInTheDocument();
+        }
+        expect(submitButton).toBeInTheDocument();
+        expect(submitButton).toBeDisabled();
+
+    },5000);
+});
+
+describe("Testing Infrastructure of Section 16 Interactions", () => {
+    it("Name TextBox Type Correctly", async () => {
+        await userEvent.type(textBoxName, "tom");
+        await userEvent.type(textBoxEmail, "tom@gmail.com");
+        expect(submitButton).toBeEnabled();
+    },5000);
 });
--- Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/components/SimpleInput.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/components/SimpleInput.tsx
@@ -3,14 +3,19 @@ import { useState, useEffect } from 'react';
 
 export const SimpleInput = () => {
     const [enteredName, setEnteredName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+    const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
     const enteredNameIsValid = enteredName.trim() !== '';
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+    const enteredEmailIsValid = enteredEmail.includes('@');
+    const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
     let formIsValid = false;
 
-    if(enteredNameIsValid) {
+    if(enteredNameIsValid && enteredEmailIsValid) {
         formIsValid = true;
     }
 
@@ -22,26 +27,46 @@ export const SimpleInput = () => {
         setEnteredNameTouched(true);
     }
 
+    const emailInputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredEmail(event.target.value);
+    }
+
+    const emailInputBlurHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+        setEnteredEmailTouched(true);
+    }
+
     const formSubmissionHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setEnteredNameTouched(true);
+        setEnteredEmailTouched(true);
 
-        if(!enteredNameIsValid) {
-            return;
+        if(!enteredNameIsValid && !enteredEmailIsValid) {
+            return false;
+        } else {
+                // Reset After Submission!
+            setEnteredName('');
+            setEnteredNameTouched(false);
+            setEnteredEmail('');
+            setEnteredEmailTouched(false);
         }
-        setEnteredName('');
-        setEnteredNameTouched(false);
+        
     };
 
 
     const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+    const emailInputClasses = emailInputIsInvalid ? 'form-control invalid' : 'form-control';
   return (
     <form onSubmit={formSubmissionHandler}>
         <div className={nameInputClasses}>
             <label htmlFor='name'>Your Name</label>
             <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
             {nameInputIsInvalid && <p>Name must not be empty.</p>}
-        </div>
+          </div>
+          <div className={emailInputClasses}>
+              <label htmlFor='email'>Your E-mail</label>
+              <input type="email" id="email" onChange={emailInputChangeHandler} onBlur={emailInputBlurHandler} value={enteredEmail} />
+              {emailInputIsInvalid && <p>Pleased Entered a valid e-mail.</p>}
+          </div>
         <div className='form-actions'>
             <button disabled={!formIsValid}>Submit</button>
         </div>

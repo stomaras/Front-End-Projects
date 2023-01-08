58fa8d977462182a365ff621dc9aa46535c0dc59
stomaras <sptoma724@gmail.com>
2023-01-08 15:27:07 +0200

    Custom Hook

---
+++ Typescript/TypescriptUdemy/ProjectUdemy/section16udemy/src/hooks/use-input.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react';
+
+export const useInput = (validateValue:any) => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isTouched, setIsTouched] = useState(false);
+
+    const valueIsValid = validateValue(enteredValue);
+    const hasError = !valueIsValid && isTouched;
+
+    const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredValue(event.target.value);
+    }
+
+    const valueInputBlurHandler = (event: React.FocusEvent<HTMLElement>) => {
+        setIsTouched(true);
+    }
+
+    const reset = () => {
+        setEnteredValue('');
+        setIsTouched(false);
+    }
+  
+    return {
+        value: enteredValue,
+        isValid: valueIsValid,
+        hasError,
+        valueChangeHandler,
+        valueInputBlurHandler,
+        reset
+    };
+}
+
+export default useInput;

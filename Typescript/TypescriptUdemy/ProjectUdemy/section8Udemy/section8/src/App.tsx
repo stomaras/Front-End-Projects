32c307d03f4634962dc9578aeb95d79ffba6c851
stomaras <sptoma724@gmail.com>
2023-01-11 18:03:01 +0200

    Section 8 Adding a re-usable Button Component

---
+++ Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './components/Users/AddUser';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser/>
+    </div>
+  );
+}
+
+export default App;

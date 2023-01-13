3e8a80da600e78c965d7d3d486c3f4f46852477e
stomaras <sptoma724@gmail.com>
2023-01-13 17:14:54 +0200

    Test done until section Managing a List of Users Via State

--- Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/App.test.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/App.test.tsx
@@ -5,36 +5,40 @@ import App from './App';
 
 
 let wrapperApp: RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>;
-let textBoxes: HTMLElement[];
 let button: HTMLElement
 let usersList: HTMLElement;
+
+let ageField: HTMLElement;
+let nameField: HTMLElement;
 
 describe("Section 8 Testing", () => {
   
   beforeEach(() => {
     render(<App />);
-    textBoxes = screen.getAllByRole('textbox');
     button = screen.getByRole('button', { name: /add user/i , hidden:true});
     usersList = screen.getByRole('list');
+    ageField = screen.getByRole('spinbutton', { name: /age \(years\)/i });
+    nameField = screen.getByRole('textbox', { name: /username/i });
   }, 5000);
 
-  test('Expect App Component to be in the document', () => {
-
-    for (let index = 0; index < textBoxes.length; index++) {
-      const textBox = textBoxes[index];
-      expect(textBox).toBeInTheDocument();
-    }
+  it('Expect App Component to be in the document', () => {
+    screen.debug(undefined, 100000);
+    expect(nameField).toBeInTheDocument();
+    expect(ageField).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(usersList).toBeInTheDocument();
-  });
+  },5000);
 
-  it("Type users - click submit - check list", async () => {
-    screen.debug(undefined, 10000);
-    await user.type(textBoxes[0], "Tom");
-    expect(textBoxes[0]).toHaveValue("Tom");
-
-    button.hidden = false;
+  it("Type name inside text box and age ", async () => {
+    await user.type(nameField, "Tom");
+    expect(nameField).toHaveValue("Tom");
+    await user.type(ageField, "25");
     await user.click(button);
-    screen.debug(undefined, 100000)
-  })
+    screen.debug(undefined, 100000);
+    expect(screen.getByRole('listitem')).toBeInTheDocument();
+    expect(screen.getByText(/tom/i)).toBeInTheDocument();
+    expect(screen.getByText(/25/i)).toBeInTheDocument();
+  },5000);
+
+  
 });

86a9a084fe9e0823098a3c173a6e7aad8e35a4c9
stomaras <sptoma724@gmail.com>
2023-01-13 17:52:05 +0200

    Error Modal Lecture Done

--- Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/UI/ErrorModal.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/UI/ErrorModal.tsx
@@ -5,17 +5,21 @@ import classes from './ErrorModal.module.css';
 
 export const ErrorModal = (props: any) => {
     return (
-        <Card className={classes.modal}>
-            <header>
-                <h2>{props.title}</h2>
-            </header>
-            <div>
-                <p>{props.message}</p>
-            </div>
-            <footer>
-                <Button>Okay</Button>
-            </footer>
-        </Card>
+        <div>
+            <div className={classes.backdrop}/>
+            <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button>Okay</Button>
+                </footer>
+            </Card>
+        </div>
+        
     );
 };
 export default ErrorModal;
--- Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/Users/AddUser.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/Users/AddUser.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props: any) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -28,17 +29,19 @@ const AddUser = (props: any) => {
     setEnteredAge(event.target.value);
   }
 
-    return (
+  return (
+    <div>
+      <ErrorModal title="An error occured!" message="Something Went Wrong!" />
       <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-            <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type="number" value={enteredAge} onChange={ageChangeHandler} />  
-            <Button type='submit'>Add User</Button>
-        </form>        
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type="number" value={enteredAge} onChange={ageChangeHandler} />
+        <Button type='submit'>Add User</Button>
+      </form>
       </Card>
-    
+    </div>    
   )
 }
 

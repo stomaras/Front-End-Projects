aca0a2359fc684a9a80405ac91385f4bd192041e
stomaras <sptoma724@gmail.com>
2023-01-14 13:45:14 +0200

    Section 8 development environment done

--- Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/Users/AddUser.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section8Udemy/section8/src/components/Users/AddUser.tsx
@@ -3,17 +3,27 @@ import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
+import { Error } from './Error';
 
 const AddUser = (props: any) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState<Error | null>();
 
     const addUserHandler = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
       if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        setError({
+          title: 'Invalid Input',
+          message: 'Please enter a valid name and age (non-empty values)'
+        });
         return;
       }
       if (Number(enteredAge) < 1) {
+        setError({
+          title: 'Invalid Age',
+          message: 'Please enter a valid age (> 0).'
+        });
         return;
       }
       props.onAddUser(enteredUsername, enteredAge);
@@ -23,15 +33,19 @@ const AddUser = (props: any) => {
   
   const usernameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredUsername(event.target.value);
-  }
+  };
 
   const ageChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredAge(event.target.value);
-  }
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
 
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something Went Wrong!" />
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>

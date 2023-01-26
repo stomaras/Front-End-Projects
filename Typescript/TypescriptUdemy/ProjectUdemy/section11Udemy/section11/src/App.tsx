966da33f27cdd955d71a8a7a74aff1d65d2a82a3
stomaras <sptoma724@gmail.com>
2023-01-26 17:54:03 +0200

    Section 11 start project

---
+++ Typescript/TypescriptUdemy/ProjectUdemy/section11Udemy/section11/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started</h2>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+UI Folder :
+- Place general UI Elements, input component, card component
+
+Layout Folder :
+- Header Component
+
+Meals Folder :
+- That's folder will hold that list of meals, but also the individual meal item components.
+
+Card Folder : 
+- Will hold all the card related components.
+
+
+
+
+*/

98e6f6e50cfe2ee9f7f119f81cd1e64fe94e93f9
stomaras <sptoma724@gmail.com>
2023-01-27 18:16:30 +0200

    Add Test

--- Typescript/TypescriptUdemy/ProjectUdemy/section11Udemy/section11/src/App.test.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section11Udemy/section11/src/App.test.tsx
@@ -4,16 +4,32 @@ import App from './App';
 
 
 let header: HTMLElement;
-let cardButton: HTMLElement
+let cardButton: HTMLElement;
+let mealsSummaryHeader: HTMLElement;
+let mealsSummaryP1: HTMLElement;
+let mealsSummaryP2: HTMLElement;
+let mealsUl: Element | HTMLDocument | (Element | HTMLDocument)[] | undefined;
+let mealsList: string | any[] | NodeListOf<HTMLLIElement>;
+
+
 beforeAll(() => {
 
 });
 
 beforeEach(() => {
+
   render(<App />);
   screen.debug(undefined, 100000);
   cardButton = screen.getByRole('button');
-},5000);
+  mealsSummaryHeader = screen.getByRole('heading', { name: /delicious food, delivered to you/i });
+  mealsSummaryP1 = screen.getByText(/choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home\./i);
+  mealsSummaryP2 = screen.getByText(/all our meals are cooked with high\-quality ingredients, just\-in\-time and of course by experienced chefs!/i);
+  mealsList = document.querySelectorAll('li');
+  mealsUl = document.querySelectorAll('ul')[0];
+  screen.debug(mealsUl);
+  screen.debug(undefined, 100000);
+
+}, 5000);
 
 describe("Section 11 Building a Food Order App Testing", () => {
   it("App to be in the document", () => {
@@ -22,5 +38,17 @@ describe("Section 11 Building a Food Order App Testing", () => {
     const image = screen.getByRole('img');
     expect(image).toBeInTheDocument();
     expect(cardButton).toBeInTheDocument();
+    expect(mealsSummaryHeader).toBeInTheDocument();
+    expect(mealsSummaryP1).toBeInTheDocument();
+    expect(mealsSummaryP2).toBeInTheDocument();
+
+    expect(mealsUl).toBeInTheDocument();
+
+    for (let i = 0; i < mealsList.length; i++) {
+      const li = mealsList[i];
+      expect(li).toBeInTheDocument();
+      const liText = screen.getByText(li.textContent)
+      expect(liText).toBeInTheDocument();
+    }
   }, 5000);
 });

22327fc5861bf598c7fd00846e9c3b02113caa71
stomaras <sptoma724@gmail.com>
2023-02-02 18:16:43 +0200

    Using the Context

--- Typescript/TypescriptUdemy/ProjectUdemy/section11Udemy/section11/src/components/Layout/HeaderCardButton.tsx
+++ Typescript/TypescriptUdemy/ProjectUdemy/section11Udemy/section11/src/components/Layout/HeaderCardButton.tsx
@@ -1,7 +1,18 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCardButton.module.css';
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props: any) => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx
+        .items
+        .reduce((curNumber, item) => {
+            return curNumber + item.amount
+        },0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -9,7 +20,7 @@ const HeaderCardButton = (props: any) => {
             </span>
             <span>Your Cart</span>
             <span>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )

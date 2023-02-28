e9de06e16555b6ab7fbe31b14685a0397f6be156
stomaras <sptoma724@gmail.com>
2023-02-28 13:01:15 +0200

    set up

---
+++ Typescript/ReactProjectsUdemy/section6/03-advanced-react/src/App.jsx
@@ -0,0 +1,11 @@
+import Starter from './tutorial/01-useState/starter'
+function App() {
+  return (
+    <div className='container'>
+      <Starter/>
+      <Final/>
+    </div>
+  );
+}
+
+export default App;

25de055fe2bc7e9933622c92da9d6b3a35ed3cf1
stomaras <sptoma724@gmail.com>
2023-03-02 16:52:06 +0200

    code example useEffect

--- Typescript/ReactProjectsUdemy/section6/my-app/src/App.jsx
+++ Typescript/ReactProjectsUdemy/section6/my-app/src/App.jsx
@@ -1,10 +1,9 @@
-import UseStateObject from './tutorial/01-useState/starter/04-useState-object';
-import UseStateGotcha from './tutorial/01-useState/starter/05-useState-gotcha';
+import Starter from "./tutorial/02-useEffect/starter/01-code-example"
 
 function App() {
   return (
     <div className='container'>
-      <UseStateGotcha/>
+      <Starter/>
     </div>
   );
 }
--- Typescript/ReactProjectsUdemy/section6/my-app/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ Typescript/ReactProjectsUdemy/section6/my-app/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react';
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  // if you update the state inside that function you will get infinite loop 
+  // useEffect help us to run code conditionally in our application
+  const sayHello = () => {
+    console.log('hello there');
+    setValue(value+1);
+  };
+
+  sayHello();
+
   return (
     <div>
       <h1>value : {value}</h1>

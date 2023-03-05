d5075b5f4b49aa3cdff1473844f902d8ada84fd5
stomaras <sptoma724@gmail.com>
2023-03-05 19:31:40 +0200

    UseEffect timer example

--- Typescript/ReactProjectsUdemy/section6/my-app/src/tutorial/03-conditional-rendering/starter/CleanUpFunction.jsx
+++ Typescript/ReactProjectsUdemy/section6/my-app/src/tutorial/03-conditional-rendering/starter/CleanUpFunction.jsx
@@ -11,9 +11,18 @@ const CleanUpFunction = () => {
   </div>;
 };
 
+// with the return clean up function run when the componennt 
+// unmount 
+// so run in the background
 const RandomComponent = () => {
   useEffect(() => {
     console.log('hmm , this is interesting');
+    const intId = setInterval(() => {
+      console.log('hello from interval');
+    }, 1000);
+    return () => {
+      clearInterval(intId);
+    }
   },[])
   return <h1>Hello There</h1>;
 }

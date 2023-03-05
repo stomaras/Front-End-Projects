cb6ccd247ef76aa87c9f9784d2fd982863c8756c
stomaras <sptoma724@gmail.com>
2023-03-05 15:45:44 +0200

    User Challenge

--- Typescript/ReactProjectsUdemy/section6/my-app/src/App.jsx
+++ Typescript/ReactProjectsUdemy/section6/my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import UseEffectBasics from "./tutorial/02-useEffect/starter/02-useEffect-basics";
 import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
 import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
-import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
+import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
 
 function App() {
   return (

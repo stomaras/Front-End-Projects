663440a3929e53556690d321e97d7d2477aa123e
stomaras <sptoma724@gmail.com>
2023-03-13 20:34:12 +0200

    multiple inputs elements

---
+++ Typescript/ReactProjectsUdemy/section6/multipletypescriptinputs/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MultipleInputs from './MultipleInputs';
+
+function App() {
+  return (
+    <div className="App">
+      <MultipleInputs/>
+    </div>
+  );
+}
+
+export default App;

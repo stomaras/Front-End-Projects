8b06626ae54bace5d835f3dbef07ef8079a7fb3f
stomaras <sptoma724@gmail.com>
2023-03-14 19:21:02 +0200

    Select Input

---
+++ Typescript/ReactProjectsUdemy/section6/checkbox/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import OtherInputs from './OtherInputs';
+
+function App() {
+  return (
+    <div className="App">
+      <OtherInputs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Typescript/ReactProjectsUdemy/section6/checkbox/src/OtherInputs.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+const frameworks = ['react', 'angular', 'vue', 'svelte'];
+
+const OtherInputs = () => {
+    const [shipping, setShipping] = useState(false);
+    const [framework, setFramework] = useState('react');
+
+    const handleShipping = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.checked);
+        setShipping(e.target.checked);
+    };
+
+    const handleFramework = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setFramework(e.target.value);
+    };
+
+  return (
+        <div>
+          <form className='form'>
+              <h4>Other Inputs</h4>
+              <div className='form-row' style={{ textAlign: 'left' }}>
+                  <label htmlFor="shipping">Free Shipping</label>
+                  <input type="checkbox" name='shipping' id='shipping' checked={shipping} onChange={handleShipping} />
+              </div>
+              <div className='form-row' style={{ textAlign: 'left' }}>
+                  <label htmlFor="framework" className='form-label'>Framework</label>
+                  <select name="framework" id="framework" value={framework} onChange={handleFramework}>
+                      {frameworks.map((framework) => {
+                          return <option key={framework}>{framework}</option>;
+                      })}
+                  </select>
+              </div>
+              <button type='submit' className='btn btn-block'>
+                  submit
+              </button>
+          </form>
+        </div>
+  )
+}
+
+export default OtherInputs

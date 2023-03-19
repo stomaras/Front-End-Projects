237ba7c034df1d35bc0722124405e78055f8cc70
stomaras <sptoma724@gmail.com>
2023-03-19 16:20:54 +0200

    UseReducer - Challenge

--- Typescript/ReactProjectsUdemy/section6/usereducer/src/UseStateArray.tsx
+++ Typescript/ReactProjectsUdemy/section6/usereducer/src/UseStateArray.tsx
@@ -7,19 +7,39 @@ const UseStateArray = () => {
     const [people, setPeople] = React.useState<any>(data);
 
     const removeItem = (id: number) => {
-        let newPeople = people.filter((person: Person) => person.id !== id);
+        // let newPeople = people.filter((person: Person) => person.id !== id);
+        // setPeople(newPeople);
+        setPeople(people.filter((person: Person) => person.id !== id));
+    };
+
+    const clearAllItems = () => {
+        setPeople([]);
+    };
+
+    const resetList = () => {
+        setPeople(data);
     }
 
   return (
       <div>
-          
           {people.map((person: any) => {
               const {id, name} = person
-              // console.log(person);
               return <div key={id}>
                   <h4>{name}</h4>
+                  <button type='button' onClick={() => removeItem(id)}>remove</button>
               </div>;  
           })}
+          {people.length < 1 ? (
+                  <button type='button' style={{ marginTop: '2rem' }} className="btn" onClick={resetList}>
+                    Reset Items
+                  </button>
+            ) : (
+                <button type='button' style={{ marginTop: '2rem' }} className="btn" onClick={clearAllItems}>
+                    Clear Items
+                </button> 
+            )
+          }
+          
     </div>
   )
 }

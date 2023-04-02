a4d8a109af73015580f97e712956e3b620194ec7
stomaras <sptoma724@gmail.com>
2023-04-02 21:20:47 +0300

    Tabs Fetch Jobs

--- Typescript/ReactProjectsUdemy/section7/tabs/src/App.tsx
+++ Typescript/ReactProjectsUdemy/section7/tabs/src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return <section className='jobs-center'>
+      <div className="loading">
+        
+      </div>
+    </section>
+  }
+  
   return (
     <div className="App">
       Tabs Start

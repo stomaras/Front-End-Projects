8d3ee82b0eedcf9342457c412a925d1d895e1974
stomaras <sptoma724@gmail.com>
2023-04-25 20:16:43 +0300

    navbar data set up project

---
+++ Typescript/ReactProjectsUdemy/section7/navbar/README.md
@@ -0,0 +1,7 @@
+## Steps
+
+#### Data
+
+Check the data.ts file and find two arrays: one for links
+and one for social icons. Notice how icons from react-icons
+can be used directly in the data structures.
---
+++ Typescript/ReactProjectsUdemy/section7/navbar/src/data.ts
@@ -0,0 +1,60 @@
+import React from 'react'
+import { FaFan, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+export interface SocialInterface {
+    id: number;
+    url: string;
+}
+
+export interface LinksInterface {
+    id: number;
+    url: string;
+    text: string;
+}
+
+export const links: LinksInterface[] = [
+  {
+    id: 1,
+    url: '/',
+    text: 'random',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+]
+
+export const social: SocialInterface[] = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+  },
+]

9416dd16559005e87b958902f9020c15029f1ae1
stomaras <sptoma724@gmail.com>
2023-07-24 20:59:26 +0300

    Selecting and Manipulating Elements

--- Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
+++ Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
@@ -1,4 +1,11 @@
 'use strict';
 
 const message = document.querySelector(".message").textContent;
-console.log(message);
+document.querySelector('.message').textContent = 'Correct Number'
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector(".guess").value;
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);

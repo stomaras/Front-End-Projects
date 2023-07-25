26e84db43db33777c9e896b4b9b8b651d8e4c5dd
stomaras <sptoma724@gmail.com>
2023-07-25 20:21:38 +0300

    Handling Click Events

--- Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
+++ Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const message = document.querySelector(".message").textContent;
-document.querySelector('.message').textContent = 'Correct Number'
+// const message = document.querySelector(".message").textContent;
+// document.querySelector('.message').textContent = 'Correct Number'
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector(".guess").value;
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value;
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+const checkButton = document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number';
+    }
+
+});

640b4d44b23f6d5ededd156d7af7fd45a91db0a1
stomaras <sptoma724@gmail.com>
2023-07-26 20:51:56 +0300

    Manipulating CSS Styles

--- Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
+++ Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
@@ -1,14 +1,27 @@
 'use strict';
 
-// const message = document.querySelector(".message").textContent;
-// document.querySelector('.message').textContent = 'Correct Number'
+/* 
+We want the secret number to be defined once only when we start the application 
+not on each click
+*/
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
+const INITIAL_SCORE = 20;
 
-// document.querySelector(".guess").value;
-// document.querySelector(".guess").value = 23;
-// console.log(document.querySelector(".guess").value);
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let scoreSpan = document.querySelector('.score');
+let highScore = document.querySelector('.highscore');
+let message = document.querySelector('.message');
+let bodyElement = document.querySelector('body');
+let numberQuestion = document.querySelector('.number');
+
+const palette = {
+    successBackgroundColor: '#25be17',
+    looseBackgroundColor: '#b80a0a'
+};
+
+let score = INITIAL_SCORE;
+
+document.querySelector('.number').textContent = secretNumber;
 
 const checkButton = document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -16,7 +29,31 @@ const checkButton = document.querySelector('.check').addEventListener('click', f
     console.log(guess, typeof guess);
 
     if(!guess){
-        document.querySelector('.message').textContent = 'No number';
-    }
+        message.textContent = 'No number';
 
+    // when player wins
+    } else if (guess === secretNumber) {
+        message.textContent = 'Correct Number';
+        bodyElement.style.backgroundColor = palette.successBackgroundColor;
+        numberQuestion.style.width = '30rem';
+    // when player lost
+    } else if(guess > secretNumber) {
+        if(score > 1){
+            message.textContent = 'Too High';
+            score--;
+            scoreSpan.textContent = score;
+        }else {
+            message.textContent = 'You Lost the game';
+            scoreSpan.textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if(score > 1){
+            message.textContent = 'Too Low';
+            score--;
+            scoreSpan.textContent = score;
+        } else {
+            message.textContent = 'You Lost the game';
+            scoreSpan.textContent = 0;
+        }
+    }
 });

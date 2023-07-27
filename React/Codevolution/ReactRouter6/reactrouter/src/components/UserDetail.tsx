bdb4fa3e089b339eca658f96741fd4f015005fce
stomaras <sptoma724@gmail.com>
2023-07-27 15:39:38 +0300

    react URL Params

--- React/Codevolution/ReactRouter6/reactrouter/src/components/UserDetail.tsx
+++ React/Codevolution/ReactRouter6/reactrouter/src/components/UserDetail.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetail = () => {
+  const params = useParams()
+  const userId = params.userId
+
   return (
     <div>
-        Details about user
+        Details about user {userId}
     </div>
   )
 }

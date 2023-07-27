e4c5b8986d5eed4d5eb0cb1df37dc00204148b48
stomaras <sptoma724@gmail.com>
2023-07-27 20:14:21 +0300

    Coding Challenge Done

--- Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
+++ Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/GuessMyNumber/starter/script.js
@@ -1,32 +1,26 @@
 'use strict';
 
-/* 
-We want the secret number to be defined once only when we start the application 
-not on each click
-*/
-
 const INITIAL_SCORE = 20;
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+const palette = {
+    successBackgroundColor: '#25be17',
+    looseBackgroundColor: '#b80a0a',
+    initialBackgroundColor: '#222'
+};
+
+let secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = INITIAL_SCORE;
+
 let scoreSpan = document.querySelector('.score');
 let highScore = document.querySelector('.highscore');
 let message = document.querySelector('.message');
 let bodyElement = document.querySelector('body');
 let numberQuestion = document.querySelector('.number');
-
-const palette = {
-    successBackgroundColor: '#25be17',
-    looseBackgroundColor: '#b80a0a'
-};
-
-let score = INITIAL_SCORE;
-
-document.querySelector('.number').textContent = secretNumber;
+const guessInput = document.querySelector('.guess');
+const buttonAgain = document.querySelector('.again');
 
 const checkButton = document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
-
-    console.log(guess, typeof guess);
 
     if(!guess){
         message.textContent = 'No number';
@@ -36,6 +30,8 @@ const checkButton = document.querySelector('.check').addEventListener('click', f
         message.textContent = 'Correct Number';
         bodyElement.style.backgroundColor = palette.successBackgroundColor;
         numberQuestion.style.width = '30rem';
+        numberQuestion.textContent = secretNumber;
+
     // when player lost
     } else if(guess > secretNumber) {
         if(score > 1){
@@ -57,3 +53,13 @@ const checkButton = document.querySelector('.check').addEventListener('click', f
         }
     }
 });
+
+
+buttonAgain.addEventListener('click', function() {
+    bodyElement.style.backgroundColor = palette.initialBackgroundColor;    
+    message.textContent = "Start Guessing...";
+    numberQuestion.textContent = "?";
+    numberQuestion.style.width = '15rem';
+    guessInput.value = "";
+    scoreSpan.textContent = 20;
+}); 

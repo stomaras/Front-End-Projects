6909532e36c0a25b06a7776e29d709ccbc58ddde
stomaras <sptoma724@gmail.com>
2023-07-27 14:16:29 +0300

    NavLink component with active css class

--- React/Codevolution/ReactRouter6/reactrouter/src/components/Navbar.tsx
+++ React/Codevolution/ReactRouter6/reactrouter/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 
 export const Navbar = () => {
   return (
     <nav className='navbar'>
-        <Link to='/' className='navbar__link'>Home</Link>
-        <Link to='/about' className='navbar__link'>About</Link>
+        <NavLink to='/' className='navbar__link'>Home</NavLink>
+        <NavLink to='/about' className='navbar__link'>About</NavLink>
     </nav>
   )
 }

344fa41b7a9f90cec49aa2d5c833e81ca15d37f2
stomaras <sptoma724@gmail.com>
2023-07-27 15:35:29 +0300

    dynamic routes

--- React/Codevolution/ReactRouter6/reactrouter/src/App.tsx
+++ React/Codevolution/ReactRouter6/reactrouter/src/App.tsx
@@ -7,6 +7,9 @@ import { NoMatch } from "./components/NoMatch";
 import { Products } from "./components/Products";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { NewProducts } from "./components/NewProducts";
+import { Users } from "./components/Users";
+import UserDetail from "./components/UserDetail";
+import { Admin } from "./components/Admin";
 
 function App() {
   return (
@@ -21,8 +24,11 @@ function App() {
           <Route path="featured" element={<FeaturedProducts/>}/>
           <Route path="new" element={<NewProducts/>}/>  
         </Route>
+        <Route path="users" element={<Users/>}>
+          <Route path=":userId" element={<UserDetail/>}/>
+          <Route path="admin" element={<Admin/>}></Route>
+        </Route>
         <Route path= "*" element={<NoMatch/>}/>
-
       </Routes>    
     </>
   );
---
+++ React/Codevolution/ReactRouter6/reactrouter/src/components/UserDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDetail = () => {
+  return (
+    <div>
+        Details about user
+    </div>
+  )
+}
+
+export default UserDetail

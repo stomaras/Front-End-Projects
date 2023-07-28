f97d2c3d161d8f47f8b314f8144c181be53d61a9
stomaras <sptoma724@gmail.com>
2023-07-28 20:11:06 +0300

    Working with classes

--- Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/ModalWindow/starter/script.js
+++ Javascript/Section7_DOM_EVENTS_FUNDAMENTALS/ModalWindow/starter/script.js
@@ -5,6 +5,17 @@ let closeModalButton = document.querySelector('.close-modal');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');    
+}
+
 modalButtons.forEach((modalButton) => {
-    console.log(modalButton.textContent);
+    modalButton.addEventListener('click',openModal);
 })
+closeModalButton.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)

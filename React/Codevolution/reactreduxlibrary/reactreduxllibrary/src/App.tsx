dc39033d5b00754f6c4c550a199882e0566d673a
stomaras <sptoma724@gmail.com>
2023-07-28 13:48:34 +0300

    react-redux

--- React/Codevolution/reactreduxlibrary/reactreduxllibrary/src/App.tsx
+++ React/Codevolution/reactreduxlibrary/reactreduxllibrary/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CakeContainer from './CakeContainer';
+import { Provider } from "react-redux"
+import store from './redux/cakes/store';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer/>
-    </div>
+    <Provider store={store}>
+        <div className="App">
+          <CakeContainer/>
+        </div>
+    </Provider>
+
   );
 }
 

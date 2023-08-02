d13dbdcd70dd1f4fc8f196c53ed2ace3e9d707b5
stomaras <sptoma724@gmail.com>
2023-08-02 11:59:22 +0300

    Creating a Bar Chart ( Groups & Margins )

--- Javascript/D3/Section4/index.js
+++ Javascript/D3/Section4/index.js
@@ -1,15 +1,28 @@
 // collect the svg container first
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+    .append('svg')
+        .attr('width', 600)
+        .attr('height', 600);
+
+// create margins and dimensions
+const margin = {top:20, right:20, bottom:100, left:100}
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`)
 
 d3.json('menu.json').then(data => {
     
     // const minOrders = d3.min(data, d => d.orders);
-    const maxOrders = d3.max(data, d => d.orders);
-    const extent = d3.extent(data, d => d.orders);
+    // const maxOrders = d3.max(data, d => d.orders);
+    // const extent = d3.extent(data, d => d.orders);
 
     // scale for y direction
     const y = d3.scaleLinear()
-        .domain([0,maxOrders])
+        .domain([0,1000])
         .range([0,500]);
 
 
@@ -21,7 +34,7 @@ d3.json('menu.json').then(data => {
         .paddingOuter(0.2);
 
     // join the data to rect
-    const rects = svg.selectAll('rect').data(data)
+    const rects = graph.selectAll('rect').data(data)
 
     rects.attr('width', x.bandwidth())
         .attr('height', d => y(d.orders))

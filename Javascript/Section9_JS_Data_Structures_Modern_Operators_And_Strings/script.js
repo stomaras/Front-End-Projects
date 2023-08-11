2666197347981fec2af5f545f38a273cdb5f8565
stomaras <sptoma724@gmail.com>
2023-08-11 17:55:34 +0300

    Destructuring Objects

--- Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
+++ Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
@@ -6,9 +6,25 @@ const restaurant = {
     categories:['Italian','Pizzeria','Vegeterian','Organic'],
     starterMenu: ['Focaccia','Bruschetta','Garlic Bread', 'Caprese Salad'],
     mainMenu:['Pizza','Pasta','Risotto'],
-
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24
+        },
+    },
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+    orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     }
 }
 
@@ -41,3 +57,29 @@ console.log(k,q,o);
 // may be useful if you get a data from an API 
 const [p = 1, b = 1, r = 1] = [8,9];
 console.log(p, b, r);
+
+/* Destructuring Objects */
+const {name, openingHours, categories} = restaurant
+console.log(name, openingHours, categories);
+
+const {
+    name: restaurantName, 
+    openingHours: hours, 
+    categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// set deault like this
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// nested objects 
+const { fri: {open: op, close: cl}} = openingHours
+console.log(op, cl);
+
+restaurant.orderDelivery({
+    time:'22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});

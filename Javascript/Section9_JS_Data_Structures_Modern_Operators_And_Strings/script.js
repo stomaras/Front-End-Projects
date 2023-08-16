d08cca712bd7f81ff18aadf39d16e95008500057
stomaras <sptoma724@gmail.com>
2023-08-16 16:52:23 +0300

    Looping Objects: Object Keys, Values and Entries

--- Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
+++ Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
@@ -244,3 +244,26 @@ for (const [i, el] of menu.entries()) {
 
 // element plus the index of the element
 console.log([...menu.entries()]);
+
+// PROPERTY NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days`;
+
+for( const day of properties){
+    openStr += `${day},`;
+}
+console.log(openStr);
+
+// PROPERTY VALUES 
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [day, {open, close}] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`);
+}

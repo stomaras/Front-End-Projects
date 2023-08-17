40138f80e3c6b3cb5f2d96ded11aa66f5bdb8252
stomaras <sptoma724@gmail.com>
2023-08-17 12:58:32 +0300

    Coding Challenge #3

--- Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/ChallengeCoding/script.js
+++ Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/ChallengeCoding/script.js
@@ -136,3 +136,29 @@ for(let x of game.scored) {
 }
 
 console.log(scorers);
+
+// Coding Challenge Sets Maps Arrays Objects 
+/* 
+Let's continue with our football betting app! This time , we have a map with a log of the events that happened during the game. The values are the events 
+themselves, and the keys are the minutes in which each event happened ( a football game has 90 minutes plus some extras time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after  45 min) of the game, like this:
+   [FIRST HALF] 17: Goal
+*/
+
+const gameEvents = new Map([
+    [17, 'GOAL'],
+    [36, 'Substitution'],
+    [47, 'GOAL'],
+    [61, 'Substitution'],
+    [64, 'Yellow Card'],
+    [69, 'Red Card'],
+    [70, 'Substitution'],
+    [72, 'Substitution'],
+    [76, 'GOAL'],
+    [80, 'GOAL'],
+    [92, 'Yellow card'],
+]);

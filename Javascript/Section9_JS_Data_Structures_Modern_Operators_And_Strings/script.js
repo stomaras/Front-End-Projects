63c5e119358e8c9a93af791f7d1731ac4909e445
stomaras <sptoma724@gmail.com>
2023-08-17 19:16:47 +0300

    Working with strings part 1 done

--- Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
+++ Javascript/Section9_JS_Data_Structures_Modern_Operators_And_Strings/script.js
@@ -432,3 +432,45 @@ console.log([...question.values()]);
 
 // Use when you simply need to map key to values
 // Use when you need keys that are not strings
+
+// Strings
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+// position at which the extraction will start 
+console.log(airline.slice(4));
+console.log(airline.slice(4,7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+
+console.log(airline.slice(-2));
+
+const checkMiddleSeat = function (seat) {
+    // B and E are middle seats
+    const s = seat.slice(-1);
+    if(s === 'B' || s === 'E') console.log("You got the middle seat");
+    else console.log("You got lucky");
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+// strings are primitives
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas').slice(1));

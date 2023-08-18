218d54252c488c1f1b40914ebb0f68d3edde9339
stomaras <sptoma724@gmail.com>
2023-08-18 18:13:03 +0300

    Functions Accepting Callback Functions

--- Javascript/Section10_A_Closer_Look_At_Functions/script.js
+++ Javascript/Section10_A_Closer_Look_At_Functions/script.js
@@ -77,3 +77,33 @@ function count() {
 
 
 */
+
+
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher Order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function() {
+    console.log('Hi');
+}
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);

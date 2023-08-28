2908b671553233b9f019ae4a806a1b5ab326c0e8
stomaras <sptoma724@gmail.com>
2023-08-28 20:35:51 +0300

    Which array method to use

--- Javascript/Section11_Arrays/script.js
+++ Javascript/Section11_Arrays/script.js
@@ -355,6 +355,58 @@ movements.sort((a, b) => {
 });
 console.log(movements);
 
+/*-----------------------------------------------*/
+/* Which Array Method to Use */
+/* 
+
+1) To mutate original array
+    - Add to original
+        - .push (end)
+        - .unshift (start)
+    - Remove from original
+        - .pop (end)
+        - .shift (start)
+        - .splice (any)
+    - Others:
+        - .reverse
+        - .sort
+        - .fill
+2) A new array
+    - Computed from original:
+        - .map (loop)
+    - Filtered using condition
+        - .filter
+    - Portion of original
+        - .slice
+    - Adding original to other
+        - .concat
+    - Flattening the original:
+        - .flat
+        - .flatMap
+3) An array index
+    - Based on value (===)
+        - .indexOf
+    - Based on test condition (<,>)
+        - .findIndex
+4) An array element
+    - Based on test condition (>,<)
+        - .find
+5) Know if array includes
+    - Based on value (===)
+    - Based on test condition (>,<)
+        - .some
+        - .every
+6) A new string
+    - Based on seperator string:
+        - .join
+7) To transform to value
+    - Based on accumulator:
+        (Boil down array to single value of any type: number, string, boolean, or even new array or object)
+8) To just loop array
+    - Based on callback:
+        .forEach (Does not create a new array just loops over it)
+*/
+
 
 
 ////////////////////////////////////////////////////////////////////////////

2b4b8e81db481e1d92e8429185bd5ab76a9a3285
stomaras <sptoma724@gmail.com>
2023-08-30 11:51:21 +0300

    Creating Dates

--- Javascript/Section12_Dates/script.js
+++ Javascript/Section12_Dates/script.js
@@ -369,3 +369,30 @@ console.log(huge + ' is really big');
 // Divisions
 console.log(10n / 3n);
 console.log(10 / 3);
+
+// Dates and Times 
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Wed Aug 30 2023 11:39:49'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+// Working with dates 
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(231232423424));
+
+future.setFullYear(2040);
+console.log(future);

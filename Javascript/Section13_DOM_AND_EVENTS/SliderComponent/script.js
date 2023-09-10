e24dba3d4173920babb8d56aef21402c38593e5d
stomaras <sptoma724@gmail.com>
2023-09-10 17:53:53 +0300

    Slider Part 1 Done

--- Javascript/Section13_DOM_AND_EVENTS/SliderComponent/script.js
+++ Javascript/Section13_DOM_AND_EVENTS/SliderComponent/script.js
@@ -7,9 +7,49 @@ style="transform: translateX(200%);"
 
 // Slider
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
 
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.5)';
-slider.style.overflow = 'visible';
+let curSlide = 0;
+// define number of slides from NodeList
+const maxSlide = slides.length;
 
-slides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * index}%)`));
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.5)';
+
+// slider.style.transform = 'scale(0.4) translateX(-1200px)';
+// slider.style.overflow = 'visible';
+
+slides.forEach((s,i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+
+// Next Slide
+
+btnRight.addEventListener('click', function() {
+  if(curSlide === maxSlide -1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * (index- curSlide)}%)`));
+
+});
+
+btnLeft.addEventListener('click', () => {
+
+  if(curSlide === 0) {
+    curSlide = maxSlide -1;
+  } else {
+    curSlide--;
+  }
+
+  slides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * (index- curSlide)}%)`));
+});
+
+
+
+
+
+
+

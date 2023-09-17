d5dfbf9472b4e346012ad242fb571e22d2725351
stomaras <sptoma724@gmail.com>
2023-09-17 18:52:43 +0300

    Section 2 Done

--- Typescript/section2/course-starting-project/app.ts
+++ Typescript/section2/course-starting-project/app.ts
@@ -6,6 +6,8 @@ function printResultMain(num: number){
     console.log("Result: " + num);
 }
 
+
+
 printResultMain(addMain(5,4));
 
 
@@ -14,3 +16,14 @@ let combineValues: (n1:number, n2:number) => number;
 combineValues = addMain;
 
 console.log(combineValues(8,8));
+
+
+function generateError(message:string, code:number): never {
+    throw {
+        message: message,
+        errorCode: code
+    };
+}
+
+const resultValue = generateError('An error occured', 500);
+console.log(resultValue);

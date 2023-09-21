ca29fe5bd6c78da1b6d25ad82867f8754e49c31d
stomaras <sptoma724@gmail.com>
2023-09-21 17:10:03 +0300

    Modern js Development intro on Module , Bundling, Transpiling,polyfilling

---
+++ Javascript/Section_17_Modules_Tooling/script.js
@@ -0,0 +1,19 @@
+/*
+all packages such as 3rd-party package are available with npm (React, jQuery etc)
+packages live on npm 
+we install 3rd party modules with npm 
+
+after all we want to build out progree ---> for production 
+
+                                                                (Babel)
+Module              (Join all modules into one file)        Convert modern js back to ES5
+Module-------------Bundling------------------------------->Transpiling/polyfilling--------------------------> Javascript Bundle
+3rd-Party
+Development
+
+-------------------------------------------------------------------------------------------------------------------------------
+Build Process                                                                                               Production
+
+
+Most commomnly build tool is webpack(bundler) or parcel(0 configuration)
+*/

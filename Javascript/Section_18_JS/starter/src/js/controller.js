5d9a17cad5265d563307f74b532bf6f486f46c26
stomaras <sptoma724@gmail.com>
2023-09-25 18:45:40 +0300

    Listening for load and hashchange events

--- Javascript/Section_18_JS/starter/src/js/controller.js
+++ Javascript/Section_18_JS/starter/src/js/controller.js
@@ -29,10 +29,15 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
   try{
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer)
 
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
 
     if(!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -148,6 +153,10 @@ const showRecipe = async function() {
 
 
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => {
+  window.addEventListener(ev, showRecipe);
+});
 
 // https://forkify-api.herokuapp.com/v2
 

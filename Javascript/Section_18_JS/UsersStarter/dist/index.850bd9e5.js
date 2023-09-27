170d1d1e2bb97a6d507eef23c787a19663552ba5
stomaras <sptoma724@gmail.com>
2023-09-27 18:44:59 +0300

    Users Starter data fetch on console

--- Javascript/Section_18_JS/UsersStarter/dist/index.850bd9e5.js
+++ Javascript/Section_18_JS/UsersStarter/dist/index.850bd9e5.js
@@ -575,20 +575,35 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"1GgH0":[function(require,module,exports) {
 console.log("TEST");
+const userContainer = document.querySelector(".user");
 const showUser = async function() {
     try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const res = await fetch("https://jsonplaceholder.typicode.com/users/");
         const dataUser = await res.json();
         if (!res.ok) throw new Error(`${dataUser.message} ${res.status}`);
-        let user = dataUser;
-        userFinal = {
-            id: user.id,
-            name: user.name,
-            username: user.username,
-            email: user.email
-        };
-        console.log(userFinal);
-        console.log(res, dataUser);
+        let users = dataUser;
+        console.log(users);
+        const usersData = users.slice(0, 6);
+        console.log(usersData);
+    // userFinal = {
+    //     id: user.id,
+    //     name: user.name,
+    //     username: user.username,
+    //     email: user.email
+    // }
+    // console.log(userFinal);
+    // console.log(res, dataUser);
+    // // renderUser
+    // const markUp = `
+    //         <h2>${userFinal.name}</h2>
+    //         <p>
+    //             <span>Name:</span>${userFinal.name}<br/>
+    //             <span>Username:</span>${userFinal.username}<br/>
+    //             <span>Email:</span>${userFinal.email}<br/>
+    //         </p>
+    // `
+    // userContainer.innerHTML = '';
+    // userContainer.insertAdjacentHTML('afterbegin', markUp)
     } catch (err) {
         console.log(err);
     }
--- Javascript/Section_18_JS/UsersStarter/js/controller.js
+++ Javascript/Section_18_JS/UsersStarter/js/controller.js
@@ -1,23 +1,41 @@
 console.log("TEST");
 
+const userContainer = document.querySelector('.user');
+
 const showUser = async function() {
     try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        const res = await fetch('https://jsonplaceholder.typicode.com/users/');
         const dataUser = await res.json();
 
         if(!res.ok) throw new Error(`${dataUser.message} ${res.status}`)
 
-        let user = dataUser;
+        let users = dataUser;
+        console.log(users);
 
-        userFinal = {
-            id: user.id,
-            name: user.name,
-            username: user.username,
-            email: user.email
-        }
-        console.log(userFinal);
+        const usersData = users.slice(0,6);
+        console.log(usersData);
 
-        console.log(res, dataUser);
+        // userFinal = {
+        //     id: user.id,
+        //     name: user.name,
+        //     username: user.username,
+        //     email: user.email
+        // }
+        // console.log(userFinal);
+
+        // console.log(res, dataUser);
+
+        // // renderUser
+        // const markUp = `
+        //         <h2>${userFinal.name}</h2>
+        //         <p>
+        //             <span>Name:</span>${userFinal.name}<br/>
+        //             <span>Username:</span>${userFinal.username}<br/>
+        //             <span>Email:</span>${userFinal.email}<br/>
+        //         </p>
+        // `
+        // userContainer.innerHTML = '';
+        // userContainer.insertAdjacentHTML('afterbegin', markUp)
     }catch(err) {
         console.log(err);
     }

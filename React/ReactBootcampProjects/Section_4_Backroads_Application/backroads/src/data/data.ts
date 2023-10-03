500a94e90191a1cd6f8747878960fbb1bc507997
stomaras <sptoma724@gmail.com>
2023-10-03 20:50:16 +0300

    Services Component refactoring

--- React/ReactBootcampProjects/Section_4_Backroads_Application/backroads/src/data/data.ts
+++ React/ReactBootcampProjects/Section_4_Backroads_Application/backroads/src/data/data.ts
@@ -10,6 +10,13 @@ export interface SocialLink {
     icon:string;
 }
 
+export interface Services {
+    id:number;
+    icon:string;
+    title:string;
+    text:string;
+}
+
 /****************************Data Structures****************************** */
 export const pageLinks:PageLink[] = [
     {id:1, href:'#home',text:'home'},
@@ -23,4 +30,10 @@ export const socialLinks:SocialLink[] = [
     {id: 1, href:'https://www.twitter.com', icon:'fab fa-facebook'},
     {id: 2, href:'https://www.twitter.com', icon:'fab fa-twitter'},
     {id: 3, href:'https://www.twitter.com', icon:'fab fa-squarespace'},
+]
+
+export const services:Services[] = [
+    {id:1, icon:'fas fa-wallet fa-fw', title:'saving money', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores officia'},
+    {id:2, icon:'fas fa-tree fa-fw', title:'endless hiking', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.'},
+    {id:3, icon:'fas fa-socks fa-fw', title:'amazing comfort', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores officia'},
 ]

195bb761ef681921dc4e5608fff6f903bed068bb
stomaras <sptoma724@gmail.com>
2023-10-05 11:10:56 +0300

    Tours Component

--- React/ReactBootcampProjects/Section_4_Backroads_Application/backroads/src/data/data.ts
+++ React/ReactBootcampProjects/Section_4_Backroads_Application/backroads/src/data/data.ts
@@ -18,6 +18,17 @@ export interface Services {
     text:string;
 }
 
+export interface Tours {
+    id:number;
+    image:string;
+    date:string;
+    title:string;
+    info:string;
+    location: string;
+    duration:number;
+    dollars:number;
+}
+
 /****************************Data Structures****************************** */
 export const pageLinks:PageLink[] = [
     {id:1, href:'#home',text:'home'},
@@ -38,3 +49,47 @@ export const services:Services[] = [
     {id:2, icon:'fas fa-tree fa-fw', title:'endless hiking', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.'},
     {id:3, icon:'fas fa-socks fa-fw', title:'amazing comfort', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores officia'},
 ]
+
+export const tours:Tours[] = [
+    {
+        id:1, 
+        image:'', 
+        date:'august 26th, 2020',
+        title:'Tibet Adventure', 
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+        location:'china',
+        duration: 6,
+        dollars: 2100
+    },
+    {
+        id:2, 
+        image:'', 
+        date:'october 1th, 2020',
+        title:'best of java', 
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+        location:'europe',
+        duration:10,
+        dollars:3200,
+    },
+    {
+        id:3, 
+        image:'', 
+        date:'september 15th, 2020', 
+        title:'explore hong kong', 
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+        location:'usa',
+        duration:34,
+        dollars: 3900,
+    },
+    {
+        id:4, 
+        image:'', 
+        date:'december 5th, 2019', 
+        title:'kenya highlights', 
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+        location:'Australia',
+        duration: 19,
+        dollars:8900
+    },
+]
+    

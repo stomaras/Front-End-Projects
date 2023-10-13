0f67b25c77d98e4f550a2cbc498eccc697ff7a24
stomaras <sptoma724@gmail.com>
2023-10-13 09:58:20 +0300

    Ternary Operator done

--- React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/shortcircuit/src/ShortCircuit.tsx
+++ React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/shortcircuit/src/ShortCircuit.tsx
@@ -3,17 +3,49 @@ import React, { useState } from 'react'
 const ShortCircuit = () => {
 
     // falsy
-    const [text, setText] = useState('');
+    const [text, setText] = useState('k');
 
     // truthy
     const [name, setName] = useState("Tom");
+    const [user, setUser] = useState({name:'John'});
+    const [isEditing, setIsEditing] = useState(false);
 
   return (
     <div>
-        <h4>FALSY OR: {text || 'hello'}</h4>
-        <h4>FALSY AND: {text && 'hello world'}</h4>
-        <h4>Truthy OR: {name || 'hello world'}</h4>
-        <h4>Truthy AND: {name && 'hello world'}</h4>
+      <h2>{text || 'default value'}</h2>
+      {text && ( 
+      <div>
+        <h2>whatever return </h2>
+        <h2>{name}</h2>
+      </div>
+      )}
+      {user && <SomeComponent name={user.name}/>}
+      <h2 style={{margin:'1rem 0'}}>Teranry Operator</h2>
+      <button className='btn'>{isEditing ? 'edit' : 'add'}</button>
+      {user ? (<div>
+        <h4>hello there user {user.name}</h4>
+      </div>):
+      (
+        <div>
+          <h4>please login</h4>
+        </div>
+      )}
+    </div>
+  )
+};
+
+
+interface SomeComponentProps {
+  name:string
+}
+
+const SomeComponent = (props: SomeComponentProps) => {
+  const {name} = props;
+
+  return (
+    <div>
+      <h2>whatever return</h2>
+      <h2>{name}</h2>
     </div>
   )
 }

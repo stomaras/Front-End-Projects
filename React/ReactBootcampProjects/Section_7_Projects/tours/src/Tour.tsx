4427d93dabca8d4317ba15ebe0de94063dbda1d3
stomaras <sptoma724@gmail.com>
2023-10-15 17:59:32 +0300

    Tours Read More done

--- React/ReactBootcampProjects/Section_7_Projects/tours/src/Tour.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/tours/src/Tour.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TourModel} from './models/Tour'
 import './App.css';
 
@@ -10,11 +10,15 @@ interface TourProps {
 
 const Tour = (props: TourProps) => {
     const {tour} = props;
+
+    const [readMore, setReadMore] = useState<boolean>(false);
   return (
     <article className="tour">
         <img src={tour.image} alt={tour.name} className="tour__image" width="309" height="300"/>
         <span className="tour__title">{tour.name}</span>
-        <p className="tour__info">{tour.info}</p>
+        <p className="tour__info">{readMore ? tour.info : `${tour.info.substring(0,200)}...`}
+        <button type='button' className='info-btn' onClick={() => setReadMore(!readMore)}>{readMore ? 'Show Less':'Read More'} </button>
+        </p>
         <button className="tour__button" onClick={() => props.removeTour(tour.id)}>Not Interested</button> 
     </article>
   )

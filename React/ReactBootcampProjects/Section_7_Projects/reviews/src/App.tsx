2cab468745b5540a99d3d1a9d710ed03452fef35
stomaras <sptoma724@gmail.com>
2023-10-16 20:40:45 +0300

    review project started First Person

---
+++ React/ReactBootcampProjects/Section_7_Projects/reviews/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import people from "./data";
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa";
+
+function App() {
+
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+
+  console.log(name);
+  return (
+    <div className="App">
+      <main>
+        <article className='review'>
+          <div className="img-container">
+            <img src={image} alt={name} className='person-img' />
+            <span className='quote-icon'>
+              <FaQuoteRight/>
+            </span>
+          </div>
+          <h4 className='author'>{name}</h4>
+          <p className='job'>{job}</p>
+          <p className='info'>{text}</p>
+        </article>
+      </main>
+    </div>
+  );
+}
+
+export default App;

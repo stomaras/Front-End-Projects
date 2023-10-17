2011d40313d31abaabc171c2e11780a69ca74918
stomaras <sptoma724@gmail.com>
2023-10-17 20:22:55 +0300

    prev and next person based on useState

--- React/ReactBootcampProjects/Section_7_Projects/reviews/src/App.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/reviews/src/App.tsx
@@ -9,7 +9,43 @@ function App() {
   const [index, setIndex] = useState(0);
   const {name, job, image, text} = people[index];
 
-  console.log(name);
+
+  const checkNumber = (number:number) => {
+    if(number > people.length -1){
+      return 0;
+    }
+
+    if(number < 0){
+      return people.length -1;
+    }
+    return number;
+  }
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      
+      return checkNumber(newIndex);
+    })
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      
+      return checkNumber(newIndex);
+    })
+  }
+
+  
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if(randomNumber === index){
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <div className="App">
       <main>
@@ -23,6 +59,15 @@ function App() {
           <h4 className='author'>{name}</h4>
           <p className='job'>{job}</p>
           <p className='info'>{text}</p>
+          <div className="btn-container">
+            <button className='prev-btn' onClick={prevPerson}>
+              <FaChevronLeft/>
+            </button>
+            <button className='prev-btn' onClick={nextPerson}>
+              <FaChevronRight/>
+            </button>
+          </div>
+          <button className='btn btn-hipster' onClick={randomPerson}>Surprise me</button>
         </article>
       </main>
     </div>

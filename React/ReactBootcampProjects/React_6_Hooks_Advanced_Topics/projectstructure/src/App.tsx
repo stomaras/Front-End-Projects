19718a62c145c3fc637a4b7334da744e36874575
stomaras <sptoma724@gmail.com>
2023-10-25 21:04:03 +0300

    Project Structure - Export Group with glean done

--- React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/projectstructure/src/App.tsx
+++ React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/projectstructure/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Home, About} from "./components/index"; 
 import Navbar from "./components/Navbar";
+import Example from './Example/Example';
 
 function App() {
   return (
     <div className="container">
         <Navbar/>
+        <Example/>
     </div>
   );
 }

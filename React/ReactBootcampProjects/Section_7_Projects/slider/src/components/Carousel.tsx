373a1582542578f95549cb9b16d2e7fb3f8452aa
stomaras <sptoma724@gmail.com>
2023-10-25 11:44:52 +0300

    Slider Current Item and Prev/Next

--- React/ReactBootcampProjects/Section_7_Projects/slider/src/components/Carousel.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/slider/src/components/Carousel.tsx
@@ -8,20 +8,37 @@ import { IPeople } from '../models/models'
 const Carousel = () => {
 
     const [people, setPeople] = useState<IPeople[]>(list);
+    const [currentPerson, setCurrentPerson] = useState(0);
 
     // useEffect(() => {
     //     setPeople(list)
     // },[people])
 
-    const prevSlide = () => {};
-    const nextSlide = () => {};
+    const prevSlide = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson - 1 + people.length) % people.length;
+            return result;
+        });
+    };
+    const nextSlide = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson + 1) % people.length;
+            return result;
+        });
+    };
 
   return (
     <section className='slider-container'>
         {people.map((person, personIndex) => {
             const {id, image, name, title, quote} = person;
             console.log(person);
-            return <article className='slide' style={{transform:`translateX(${100 * personIndex}%)`}} key={id}>
+            return <article className='slide' style={
+                {transform:`translateX(${100 * (personIndex - currentPerson)}%)`,
+                opacity: personIndex === currentPerson ? 1 : 0,
+                visibility: personIndex === currentPerson ? 'visible' : 'hidden'
+
+                }
+                } key={id}>
                 <img src={image} alt={name} className='person-img' />
                 <h5 className="name">{name}</h5>
                 <p className='title'>{title}</p>

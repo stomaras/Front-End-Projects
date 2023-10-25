a49efc7641ef335a2f58e123b559477ed8639d01
stomaras <sptoma724@gmail.com>
2023-10-25 10:16:33 +0300

    slider initial logic

--- React/ReactBootcampProjects/Section_7_Projects/slider/src/components/Carousel.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/slider/src/components/Carousel.tsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { longList, shortList, list } from '../data'
+import {FaQuoteRight} from "react-icons/fa";
+import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
 import { IPeople } from '../models/models'
+
 
 const Carousel = () => {
 
     const [people, setPeople] = useState<IPeople[]>(list);
 
+    // useEffect(() => {
+    //     setPeople(list)
+    // },[people])
+
+    const prevSlide = () => {};
+    const nextSlide = () => {};
+
   return (
     <section className='slider-container'>
-
+        {people.map((person, personIndex) => {
+            const {id, image, name, title, quote} = person;
+            console.log(person);
+            return <article className='slide' style={{transform:`translateX(${100 * personIndex}%)`}} key={id}>
+                <img src={image} alt={name} className='person-img' />
+                <h5 className="name">{name}</h5>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon'/>
+            </article>
+        })}
+        <button type='button' className='prev' onClick={prevSlide}><FiChevronLeft/></button>
+        <button type='button' className='next' onClick={nextSlide}><FiChevronRight/></button>
     </section>
   )
 }

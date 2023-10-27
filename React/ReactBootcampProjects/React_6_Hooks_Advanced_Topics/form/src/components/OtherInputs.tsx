ed8c3e9a5eec8c589d63bf1a396bc2e66945a5c9
stomaras <sptoma724@gmail.com>
2023-10-27 18:43:57 +0300

    Select Input

--- React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/form/src/components/OtherInputs.tsx
+++ React/ReactBootcampProjects/React_6_Hooks_Advanced_Topics/form/src/components/OtherInputs.tsx
@@ -6,10 +6,15 @@ const frameworks = ['react','angular','vue','svelte'];
 const OtherInputs = () => {
     
     const [shipping, setShipping] = useState(false);
+    const [framework, setFramework] = useState('react');
 
     const handleShipping = (e:React.ChangeEvent<HTMLInputElement>) => {
         console.log(e.target.checked);
         setShipping(e.target.checked);
+    }
+
+    const handleFramewrok = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        setFramework(e.target.value);
     }
 
   return (
@@ -24,6 +29,11 @@ const OtherInputs = () => {
                 <label htmlFor="framework" className='form-label'>
                     Framework
                 </label>
+                <select name="framework" id="framework" value={framework} onChange={handleFramewrok}>
+                    {frameworks.map((framework) => {
+                        return <option key={framework}>{framework}</option>
+                    })}
+                </select>
             </div>
             <button type='submit' className='btn btn-block'>
                 submit

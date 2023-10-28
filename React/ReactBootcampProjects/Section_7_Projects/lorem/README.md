fa21302152aa11eb3190f4273f6f7eb41d50e266
stomaras <sptoma724@gmail.com>
2023-10-28 15:48:19 +0300

    lorem Submit form

---
+++ React/ReactBootcampProjects/Section_7_Projects/lorem/README.md
@@ -0,0 +1,6 @@
+### Form
+
+Set up a form element that contains a number input
+and a submit button. Attach the count state value to the input using the value prop,
+and set up a handleChange function to update the count state value when the input changes.
+Set the minimum value to 1, the maximum value to 8, and the step to 1.
---
+++ React/ReactBootcampProjects/Section_7_Projects/lorem/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import data from "../src/data/data";
+
+function App() {
+
+  const [count, setCount] = useState<number>(1);
+  const [text, setText] = useState(data);
+
+  const handleChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(e.target.value));
+  }
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let amount = count;
+    setText(data.slice(0,amount))
+
+
+  }
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className='lorem-form' action="" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input type="number" name="amount" id="amount" min='1' max='8' step='1' value={count} onChange={handleChangeInput} />
+        <button className='btn' type='submit'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })
+
+        }
+      </article>
+    </section>
+    );
+}
+
+export default App;

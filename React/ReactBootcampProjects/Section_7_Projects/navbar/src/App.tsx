febdd2f675d6c92211b19edd5f6d2084a881ec53
stomaras <sptoma724@gmail.com>
2023-10-31 20:07:10 +0200

    navbar initial set up

---
+++ React/ReactBootcampProjects/Section_7_Projects/navbar/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from "./assets/react.svg";
+
+import './App.css'
+import { ILink } from './models';
+
+function App() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleShowLinks = () => {
+    setShowLinks(!showLinks);
+  }
+
+  return ( <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className='logo' />
+          <button className="nav-toggle" onClick={toggleShowLinks}>
+            <FaBars/>
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+          <ul className="links">
+            {links.map((link:ILink) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        )}
+        
+      </div>
+    </nav>
+  )
+}
+
+export default App;
---
+++ React/ReactBootcampProjects/Section_7_Projects/navbar/src/models.ts
@@ -0,0 +1,11 @@
+export interface ISocialLink {
+    id:number;
+    url:string;
+    icon:any;
+}
+
+export interface ILink {
+    id:number;
+    url:string;
+    text:string;
+}

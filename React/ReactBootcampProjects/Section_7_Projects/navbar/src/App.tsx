7feb91cde7301091097f1455a5d1746e0a1d95ec
stomaras <sptoma724@gmail.com>
2023-11-01 18:54:52 +0200

    navbar dynamic set up

--- React/ReactBootcampProjects/Section_7_Projects/navbar/src/App.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/navbar/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
 import { FaBars } from 'react-icons/fa';
@@ -10,12 +10,20 @@ import { ILink } from './models';
 
 function App() {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef<HTMLDivElement | null>(null);
+  const linksRef = useRef<HTMLUListElement | null>(null);
 
   const toggleShowLinks = () => {
+    console.log(showLinks);
     setShowLinks(!showLinks);
   }
 
-  return ( <nav>
+  const linkStyles :any= {
+    height:showLinks ? `${linksRef?.current?.getBoundingClientRect().height}px` : '0px'
+  }
+
+  return ( 
+  <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" className='logo' />
@@ -23,8 +31,8 @@ function App() {
             <FaBars/>
           </button>
         </div>
-          <div className={showLinks ? 'links-container show-container' : 'links-container'}>
-          <ul className="links">
+          <div className='links-container' ref={linksContainerRef} style={linkStyles}>
+          <ul className="links" ref={linksRef}>
             {links.map((link:ILink) => {
               return (
                 <li key={link.id}>

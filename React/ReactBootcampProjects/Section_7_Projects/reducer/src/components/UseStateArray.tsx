684982104280e7b39cca17363f649961e21a9a8a
stomaras <sptoma724@gmail.com>
2023-11-06 19:46:12 +0200

    reducer initial set up done

--- React/ReactBootcampProjects/Section_7_Projects/reducer/src/components/UseStateArray.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/reducer/src/components/UseStateArray.tsx
@@ -4,12 +4,28 @@ import { Person } from '../models/models';
 
 const UseStateArray = () => {
     const [people, setPeople] = React.useState<Person[]>(data);
+
+    const removeItem = (id:number) => {
+        const newPeople = people.filter((person) => person.id !== id);
+        setPeople(newPeople);
+    };
+    const clearItems = () => {
+        setPeople([]);
+    };
+
+
+    const personItems = people.map(({id,name}) => {
+        return <div>
+            <h4 key={id}>{name}</h4>
+            <button type='button' style={{marginTop:'2rem'}} className='btn' onClick={() => removeItem(id)}>remove</button>
+        </div> 
+    });
+
+    
   return (
     <div>
-        {people.map((person) => {
-            const {id, name} = person
-            return <h4 key={id}>{name}</h4>
-        })}
+        {personItems}
+        <button type='button' style={{marginTop:'2rem'}} className='btn' onClick={clearItems}>clear items</button>
     </div>
   )
 }

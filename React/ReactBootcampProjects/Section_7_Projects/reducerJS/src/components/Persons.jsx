123bd03a3580cff012f8000d26d69d4504424c8f
stomaras <sptoma724@gmail.com>
2023-11-08 00:25:16 +0200

    reducer basics with javascript

--- React/ReactBootcampProjects/Section_7_Projects/reducerJS/src/components/Persons.jsx
+++ React/ReactBootcampProjects/Section_7_Projects/reducerJS/src/components/Persons.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { data } from '../data/data'
 import { useState } from 'react'
 
+const defaultState = {
+    people:data,
+    isLoading:false,
+}
+
+const reducer = (state, action) => {
+    if(action.type === 'CLEAR_LIST'){
+        return {...state,people:[]};
+    }
+};
+
+// with useReducer we do not immediately update the state 
+// you dispatch pass the action and go through the reducer in order to update the state
 
 const Persons = () => {
-    const [people, setPeople] = useState(data);
-
+    const [state, dispatch] = useReducer(reducer,defaultState);
 
     const removeItem = (id) => {
-        const newPeople = people.filter((person) => person.id !== id);
-        setPeople(newPeople);
+        // const newPeople = people.filter((person) => person.id !== id);
+        // setPeople(newPeople);
     }
 
     const clearAllItems = () => {
-        setPeople([]);
+        dispatch({type:'CLEAR_LIST'})
     }
 
+    const resetList = () => {
+        // setPeople(data);
+    }
+
+    console.log(state);
 
     return (
         <div>
-            {people.map((person) => {
+            {state.people.map((person) => {
                 const {id, name} = person;
                 return (
                     <div key={id}>
@@ -28,9 +45,13 @@ const Persons = () => {
                     </div>
                 );
             })}
-            <button type='button' style={{marginTop:'2rem'}} className='btn' onClick={clearAllItems}>clear items</button>
+            {state.people.length < 1 ? (
+                <button type='button' style={{marginTop:'2rem'}} className='btn' onClick={() => resetList()}>reset</button>
+            ):(
+                <button type='button' style={{marginTop:'2rem'}} className='btn' onClick={() => clearAllItems()}>clear items</button>
+            )}
         </div>
-  )
+    )
 }
 
 export default Persons
--- React/ReactBootcampProjects/Section_7_Projects/reducerJS/src/data/data.js
+++ React/ReactBootcampProjects/Section_7_Projects/reducerJS/src/data/data.js
@@ -1,4 +1,6 @@
 export const data = [
     {id:1, name:'tom'},
-    {id:2, name:"agatha"}
+    {id:2, name:"agatha"},
+    {id:3, name:"tolias"},
+    {id:4, name:'marianthi'}
 ]

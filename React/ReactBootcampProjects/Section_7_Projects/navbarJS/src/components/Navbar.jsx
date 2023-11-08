400c3ab0a7f85a2f7c120a22faec3658bb5e62d1
stomaras <sptoma724@gmail.com>
2023-11-08 20:35:02 +0200

    Navbar js Initial set up css

---
+++ React/ReactBootcampProjects/Section_7_Projects/navbarJS/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links } from '../data/data';
+import logo from "../logo.svg";
+
+// import {logo} from "../assets/"
+
+const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const toggleLinks = () => {
+      setShowLinks(!showLinks);
+    }
+
+  return (
+    <nav>
+        <div className='nav-center'>
+            <div className='nav-header'>
+                <img src={logo} alt='logo' className='logo'/>
+                <button className='nav-toggle' onClick={toggleLinks}><FaBars/></button>
+            </div>
+            {showLinks && 
+                <div className="links-container">
+                  <ul className="links">
+                      {links.map((link) => {
+                      const {id, url, text} = link;
+                        return <li key={id}>
+                              <a href={url}>{text}</a>
+                        </li>
+                      })}
+                    </ul>
+                </div>            
+            }
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

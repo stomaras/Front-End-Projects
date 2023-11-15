f0167ee5a5aa389f14db3b8c1aab17914e45d5ea
stomaras <sptoma724@gmail.com>
2023-11-15 23:51:12 +0200

    shopping cart 1 hour remaining on part 2

--- React/ReactBootcampProjects/Section_7_Projects/shoppingCartYutube/src/App.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/shoppingCartYutube/src/App.tsx
@@ -2,34 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart/> : <ProductList/>
+
+  const content = (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header viewCart={viewCart} setViewCart={setViewCart}/>
+      {pageContent}
+      <Footer viewCart={viewCart}/>
     </>
   )
+
+
+  return content;
 }
 
 export default App

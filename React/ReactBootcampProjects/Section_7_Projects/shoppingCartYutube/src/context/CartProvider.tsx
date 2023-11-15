b0d4e5f2a358a137b46dabcaa19557d12b3a99e8
stomaras <sptoma724@gmail.com>
2023-11-15 23:35:58 +0200

    shopping Cart Yutube 1/2 done

--- React/ReactBootcampProjects/Section_7_Projects/shoppingCartYutube/src/context/CartProvider.tsx
+++ React/ReactBootcampProjects/Section_7_Projects/shoppingCartYutube/src/context/CartProvider.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useReducer } from "react";
+import { ReactElement, createContext, useMemo, useReducer } from "react";
 
 export interface CartItemType  {
     sku:string;
@@ -95,4 +95,41 @@ const useCartContext = (initCartState: CartStateType) => {
             return previousValue + (cartItem.qty * cartItem.price)
         }, 0)
     );
+
+
+    const cart = state.cart.sort((a,b) => {
+        const itemA = Number(a.sku.slice(-4))
+        const itemB = Number(a.sku.slice(-4))
+        return itemA - itemB;
+    });
+
+    return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart }
 }
+
+export type UseCartContextType = ReturnType<typeof useCartContext>
+
+
+
+const initCartContextState: UseCartContextType = {
+    dispatch: () => {},
+    REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+    totalItems:0,
+    totalPrice:'',
+    cart:[]
+};
+
+export const CartContext = createContext<UseCartContextType>(initCartContextState);
+
+
+type ChildrenType = { children?: ReactElement | ReactElement[]}
+
+export const CartProvider = ({children}: ChildrenType):ReactElement => {
+    return (
+        <CartContext.Provider value={useCartContext(initCartState)}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartContext;
+

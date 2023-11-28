8c3e529f58b5770234fab6eca297eca4cb7e1e64
stomaras <sptoma724@gmail.com>
2023-11-28 22:11:33 +0200

    Contentful CMS done

--- React/ReactBootcampProjects/Section_11/ContentfulCMS/src/Projects.tsx
+++ React/ReactBootcampProjects/Section_11/ContentfulCMS/src/Projects.tsx
@@ -4,8 +4,32 @@ import {useFetchProjects} from './fetchProjects';
 const Projects = () => {
     const {loading, projects} = useFetchProjects();
     console.log(loading,projects);
+
+    if(loading){
+      return <section className='projects'>
+        <h2>Loading...</h2>
+      </section>
+    }
+
   return (
-    <h2>Projects</h2>
+    <section className="projects">
+      <div className="title">
+        <h2>projects title</h2>
+        <div className="title-underline">
+        </div>
+        <div className="projects-center">
+          {
+            projects.map((project: { id: any; img: any; url: any; title: any; }) => {
+              const {id,img,url,title} = project
+              return <a key={id} href={url} target='_blank' className='project'>
+                <img src={img} alt={title} className='img' />
+                <h5>{title}</h5>
+              </a>
+            })
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 

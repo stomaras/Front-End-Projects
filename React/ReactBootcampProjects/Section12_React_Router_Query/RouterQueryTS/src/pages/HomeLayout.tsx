975a4499d275f03f123c3ac5068572496e87a8d6
stomaras <sptoma724@gmail.com>
2023-11-30 20:47:49 +0200

    RouterQueryTS Global Loading

--- React/ReactBootcampProjects/Section12_React_Router_Query/RouterQueryTS/src/pages/HomeLayout.tsx
+++ React/ReactBootcampProjects/Section12_React_Router_Query/RouterQueryTS/src/pages/HomeLayout.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import { useNavigation } from "react-router-dom";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+
   return (
     <div>
         <Navbar/>
-        <Outlet/>
+        <section>
+        {isPageLoading ? (
+          <div className='loading'></div>
+        ):(
+          <Outlet/>
+        )}
+        </section>
     </div>
   )
 }

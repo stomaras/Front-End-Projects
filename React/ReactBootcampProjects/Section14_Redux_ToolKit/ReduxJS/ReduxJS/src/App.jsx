12854a195f9f06f2a9b32eba0dfe92397b47671e
stomaras <sptoma724@gmail.com>
2023-12-01 17:05:46 +0200

    ReduxJS Reducers - Calculate Totals

--- React/ReactBootcampProjects/Section14_Redux_ToolKit/ReduxJS/ReduxJS/src/App.jsx
+++ React/ReactBootcampProjects/Section14_Redux_ToolKit/ReduxJS/ReduxJS/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from './features/cart/cartSlice';
 import './App.css'
 import {Navbar} from "./components/Navbar";
 import CartContainer from './components/CartContainer';
 
 function App() {
+
+  const {cartItems} = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  },[cartItems]);
 
   return (
     <>

bf5edb19b2705ceb63ffc84ed9ac1dfc2cb78a55
stomaras <sptoma724@gmail.com>
2023-12-04 20:35:19 +0200

    react and typescript initial set up done

--- React/ReactBootcampProjects/Section14_Redux_ToolKit/React_Typescript/react_typescript/src/components/RepositoriesList.tsx
+++ React/ReactBootcampProjects/Section14_Redux_ToolKit/React_Typescript/react_typescript/src/components/RepositoriesList.tsx
@@ -4,12 +4,13 @@ import { useDispatch } from 'react-redux';
 import { actionCreators } from '../state/index';
 import { useActions } from '../hooks/useActions';
 import { useSelector } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 
 const RepositoriesList:React.FC = () => {
     const [term, setTerm] = useState('');
     const {searchRepositories} = useActions();
-    const {data, error, loading} = useSelector((state:any) => state.repositories);
+    const {data, error, loading} = useTypedSelector((state) => state.repositories);
 
     const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -23,6 +24,9 @@ const RepositoriesList:React.FC = () => {
         <input value={term} onChange={(e) => {setTerm(e.target.value)}}/>
         <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>Loading...</h3>}
+    {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
   </div>
 };
 

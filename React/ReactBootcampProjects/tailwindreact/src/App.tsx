fc9f34fa3ca30c15ef828107c91b717434fa2fb9
stomaras <sptoma724@gmail.com>
2023-12-07 14:07:04 +0200

    Projects Section done

--- React/ReactBootcampProjects/tailwindreact/src/App.tsx
+++ React/ReactBootcampProjects/tailwindreact/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 import Skills from './components/Skills'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero/>
       <Skills/>
       <About/>
+      <Projects/>
     </>
   )
 }

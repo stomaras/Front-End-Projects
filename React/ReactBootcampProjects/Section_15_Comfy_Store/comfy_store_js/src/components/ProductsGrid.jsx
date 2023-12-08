49212687f9a839b4c0d7a7441c3a380c18cb7ad9
stomaras <sptoma724@gmail.com>
2023-12-08 11:03:32 +0200

    Format Price

--- React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_js/src/components/ProductsGrid.jsx
+++ React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_js/src/components/ProductsGrid.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link, useLoaderData} from "react-router-dom";
+import { formatPrice } from '../utils';
 
 const ProductsGrid = () => {
     const {products} = useLoaderData();
@@ -10,6 +11,7 @@ const ProductsGrid = () => {
     <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
         {products.map((product) => {
             const {title,price,image} = product.attributes;
+            const dollarsAmount = formatPrice(price);
             return <Link key={product.id} to={`/products/${product.id}`} className='cart w-full shadow-xl hover:shadow-2xl transition duration-300'>
                 <figure className='px-4 pt-4'>
                     <img src={image} alt={title} className='rounded-xl h-64 md:h-48 w-full object-cover' />
@@ -18,7 +20,7 @@ const ProductsGrid = () => {
                     <h2 className='card-title capitalize tracking-wider'>
                         {title}
                     </h2>
-                    <span className='text-secondary'>{price}</span>
+                    <span className='text-secondary'>{dollarsAmount}</span>
                 </div>
             </Link>
         })}
--- React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_js/src/utils/index.js
+++ React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_js/src/utils/index.js
@@ -7,3 +7,11 @@ const baseURL = 'https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
     baseURL: baseURL
 });
+
+export const formatPrice = (price) => {
+    const dollarsAmount = new Intl.NumberFormat('en-US', {
+        style:'currency',
+        currency:'USD',
+    }).format((price/100).toFixed(2))
+    return dollarsAmount;
+}

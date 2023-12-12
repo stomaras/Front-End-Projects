a27b2fc36e726b9a59747f056df88b24c8b5aa14
stomaras <sptoma724@gmail.com>
2023-12-12 19:58:05 +0200

    Single Product Color with ts

--- React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_ts/comfy_store_ts/src/pages/SingleProduct.tsx
+++ React/ReactBootcampProjects/Section_15_Comfy_Store/comfy_store_ts/comfy_store_ts/src/pages/SingleProduct.tsx
@@ -19,6 +19,8 @@ const SingleProduct = () => {
   const {image, title, price, description, colors, company} = product.attributes
 
   const dollarsAmount = formatPrice(price)
+  const [productColor, setProductColor] = useState(colors[0])
+
 
   return (
     <section>
@@ -44,8 +46,20 @@ const SingleProduct = () => {
           </h4>
           <p className='mt-6 leading-8'>{description}</p>
           <p className='mt-3 text-xl'>{dollarsAmount}</p>
+          {/*COLORS*/}
+          <div className='mt-6'>
+            <h4 className='text-md font-medium tracking-wider capitalize'>
+              colors
+            </h4>
+            <div className="mt-2">
+              {colors.map((color) => {
+                return <button key={color} type='button' className={`badge w-6 h-6 mr-2 ${color === productColor && 'border-2 border-secondary'}`} style={{backgroundColor:color}} onClick={() => setProductColor(color)}></button>
+              })}
+            </div>
+          </div>
         </div>
       </div>
+
     </section>
   )
 }

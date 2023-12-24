f7a6fb9d69ecd510642e1ce1cbc1918f54268e80
stomaras <sptoma724@gmail.com>
2023-12-24 21:04:32 +0200

    set up provider and consumer

---
+++ firebasereacttype/src/context/AuthContext.ts
@@ -0,0 +1,4 @@
+import React, { createContext } from "react";
+import firebase from "firebase/compat/app";
+
+export const AuthContext = createContext<firebase.User | null>(null);
---
+++ firebasereacttype/src/utils/firebaseSetup.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSi0GGp3BTl7vdbE7mjLLFKWrBbgHMQtI",
+  authDomain: "fir-auth-tut-e6e28.firebaseapp.com",
+  projectId: "fir-auth-tut-e6e28",
+  storageBucket: "fir-auth-tut-e6e28.appspot.com",
+  messagingSenderId: "679868252364",
+  appId: "1:679868252364:web:17e7e67d7321c44d794269"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();

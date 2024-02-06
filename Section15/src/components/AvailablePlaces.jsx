3da405ffd332c1b03fa121dbc3291ab7e3543eac
stomaras <sptoma724@gmail.com>
2024-02-06 22:10:29 +0200

    using async await section 15

--- Section15/src/components/AvailablePlaces.jsx
+++ Section15/src/components/AvailablePlaces.jsx
@@ -1,10 +1,27 @@
 import Places from './Places.jsx';
+import { useState, useEffect } from 'react';
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    
+    const fetchPlaces = async() => {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
+
+        
+  },[])
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />

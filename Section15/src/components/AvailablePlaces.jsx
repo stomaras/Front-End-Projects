9fe17c344af844297591a0af4b9f5b77107a1144
stomaras <sptoma724@gmail.com>
2024-02-07 21:54:08 +0200

    Sending data to back end with post requests

--- Section15/src/components/AvailablePlaces.jsx
+++ Section15/src/components/AvailablePlaces.jsx
@@ -1,25 +1,36 @@
 import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
 
-
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     
     const fetchPlaces = async() => {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if(!response.ok){
+          throw new Error('Failed to fetch places');
+        } 
+        setAvailablePlaces(resData.places);
+      }catch(error){
+        setError(error);
+      }
       setIsFetching(false);
     }
-    fetchPlaces();
 
-        
+    fetchPlaces();
   },[])
 
+  if(error){
+    return <p>Error message</p>
+  }
 
   return (
     <Places

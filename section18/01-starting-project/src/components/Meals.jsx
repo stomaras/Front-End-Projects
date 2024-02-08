9021dc5865ef66559fca05118ccbdae72e66d6d8
stomaras <sptoma724@gmail.com>
2024-02-08 17:32:43 +0200

    section 18 begins

---
+++ section18/01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import MealItem from './MealItem';
+
+const Meals = () => {
+
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+
+            try{
+                const response = await fetch('http://localhost:3000/meals');
+    
+                if(!response.ok){
+                    //
+                }
+        
+                const meals = await response.json();
+                setLoadedMeals(meals);
+            }catch(error){
+                console.log("error");
+            }
+        }
+
+        fetchMeals();
+        console.log(loadedMeals);
+    },[loadedMeals]);
+
+  return (
+    <ul id='meals'>
+        {loadedMeals.length > 0 && loadedMeals.map((meal) => {
+            return (
+                <MealItem
+                    key={meal.id}
+                    meal={meal}
+                />
+            )
+        })}
+    </ul>
+  )
+}
+
+export default Meals

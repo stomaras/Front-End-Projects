9215e376812f3331e48cac4c08dd1a71d6e01c42
stomaras <sptoma724@gmail.com>
2024-02-29 19:04:32 +0200

    Loader Component for Current User

---
+++ ReactAdvancedConcepts/ComponentPatterns/Section33/codes/src/components/current-user-loader.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import React from "react";
+
+export const CurrentUserLoader = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get('/current-user');
+            setUser(response.data);
+        })();
+    },[]);
+
+    return (
+        <>
+            {React.Children.map(children, child => {
+                if(React.isValidElement(child)){
+                    return React.cloneElement(child, {user});
+                }
+
+                return child;
+            })}
+        </>
+    );
+};

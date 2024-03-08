e92de9508e2f753ba92cb2269b3d218cba9206d3
stomaras <sptoma724@gmail.com>
2024-03-08 17:44:56 +0200

    Refactoring Passing States and Events

---
+++ ReactAdvancedConcepts/ComponentPatterns/Section_24/codes-before/src/components/books/books.tsx
@@ -0,0 +1,44 @@
+import { ChangeEventHandler, FormEventHandler, PropsWithChildren, useState } from "react";
+import "./books.css";
+import { Book, fetchBooks } from "../../App";
+
+export type BooksProps = {
+  setBooks:React.Dispatch<React.SetStateAction<Book[]>>;
+}
+
+export const Books = ({ children, setBooks }: PropsWithChildren<BooksProps>) => {
+
+  const [count, setCount] = useState(10)
+
+  return (
+    <section className="book-list gap-8">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchBooks(count).then(setBooks);
+        }}
+      >
+        <label htmlFor="number-of-books-to-load" className="book-form-label">
+          Number of Books to Load
+        </label>
+        <div className="flex">
+          <input
+            id="number-of-books-to-load"
+            className="book-input w-full"
+            type="number"
+            min="0"
+            max="20"
+            value={count}
+            onChange={(e) => setCount(e.target.valueAsNumber)}
+          />
+          <button type="submit" className="book-button">
+            Load Books
+          </button>
+        </div>
+      </form>
+      <div className="book-grid">{children}</div>
+    </section>
+  );
+};
+
+export default Books;

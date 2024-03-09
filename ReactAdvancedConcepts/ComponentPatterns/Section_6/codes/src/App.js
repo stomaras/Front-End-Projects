3eaf19d8142698224fbf984e15028ca5cdfd3293
stomaras <sptoma724@gmail.com>
2024-03-09 18:49:35 +0200

    Section 6 Custom Hooks begins

---
+++ ReactAdvancedConcepts/ComponentPatterns/Section_6/codes/src/App.js
@@ -0,0 +1,15 @@
+import { UserInfo } from "./components/user-info";
+
+function App() {
+  return (
+    <>
+      <UserInfo userId={"1"} />
+      <UserInfo userId={"2"} />
+      <UserInfo userId={"3"} />
+
+
+    </>
+  );
+}
+
+export default App;

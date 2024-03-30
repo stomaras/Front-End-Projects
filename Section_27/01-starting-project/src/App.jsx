732e66105de47f23cbbebadd3a0ccd2309a30781
stomaras <sptoma724@gmail.com>
2024-03-30 16:22:19 +0200

    Introducing Compound Components

--- Section_27/01-starting-project/src/App.jsx
+++ Section_27/01-starting-project/src/App.jsx
@@ -1,5 +1,27 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return <main>
+    <section>
+      <h2>Why work with us?</h2>
+
+      <Accordion className="accordion">
+        <AccordionItem className="accordion-item" title="We got 20 years of experience">
+          <article>
+            <p>Tou can not go wrong with us</p>
+            <p>We are in the business of planning highly vacation trips for more than 20 years</p>
+          </article>
+        </AccordionItem>
+        <AccordionItem className="accordion-item" title="We are working with local guides">
+          <article>
+            <p>Tou can not go wrong with us</p>
+            <p>We are in the business of planning highly vacation trips for more than 20 years</p>
+          </article>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  </main>;
 }
 
 export default App;
---
+++ Section_27/01-starting-project/src/components/Accordion/AccordionItem.jsx
@@ -0,0 +1,6 @@
+export default function AccordionItem({className, title, children}) {
+    return <li className={className}>
+        <h3>{title}</h3>
+        <div>{children}</div>
+    </li>
+}

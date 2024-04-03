2fcc9360fa6006e3f1629f7b6583b507283f6c38
stomaras <sptoma724@gmail.com>
2024-04-03 19:17:36 +0300

    Reacting to Events

---
+++ Section_3/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+export default function TabButton({children}) {
+    function handleClick() {
+        console.log('Hello World');
+    }
+    return <li><button onClick={handleClick}>{children}</button></li>
+}

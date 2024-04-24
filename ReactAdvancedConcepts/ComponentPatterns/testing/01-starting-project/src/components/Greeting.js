37ed88fd02cabcb8f001e2a1e4e2cbe65bf8bafc
stomaras <sptoma724@gmail.com>
2024-04-24 20:04:12 +0300

    Testing User Interaction and State

--- ReactAdvancedConcepts/ComponentPatterns/testing/01-starting-project/src/components/Greeting.js
+++ ReactAdvancedConcepts/ComponentPatterns/testing/01-starting-project/src/components/Greeting.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Greeting = () => {
+
+    const [changedText, setChangedText] = useState(false);
+
+    const changedTextHandler = () => {
+        setChangedText(true);
+    }
+
   return (
     <div>
         <h2>Hello World!</h2>
-        <p>It's good to see you!</p>
+        {!changedText && <p>It's good to see you!</p>}
+        {changedText && <p> Changed! </p>}
+        <button onClick={changedTextHandler}>Change Text!</button>
     </div>
   );
 };

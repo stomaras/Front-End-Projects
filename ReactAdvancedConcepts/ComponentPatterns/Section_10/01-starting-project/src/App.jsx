ba5282f5e9d05a43dcf68e494f89ebb87949a9a1
stomaras <sptoma724@gmail.com>
2024-05-09 18:02:27 +0300

    Consuming the Context

--- ReactAdvancedConcepts/ComponentPatterns/Section_10/01-starting-project/src/App.jsx
+++ ReactAdvancedConcepts/ComponentPatterns/Section_10/01-starting-project/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
+import { CartContext } from './store/shopping-cart-context.jsx';
+
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -66,13 +69,19 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider value={{items:[]}}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext.Provider>
   );
 }
 
